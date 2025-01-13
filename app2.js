12506bb930f82617ac21616b7ca4725ae2a861cf
simon04PL <szymon.msa@gmail.com>
2025-01-13 12:27:19 +0100

    13-01-2025 training event emmiter

--- app2.js
+++ app2.js
@@ -2,6 +2,40 @@ const EventEmitter = require('events');
 
 const eventEmmiter = new EventEmitter();
 
-eventEmmiter.on('tutorial1', () => {
-    console.log('tutorial event has occurred');
+//emiter function
+eventEmmiter.on('tutorial', (num1, num2) => {
+    console.log(num1 + num2);
 });
+
+eventEmmiter.emit('tutorial',1,2);
+
+
+//emmiter class
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+//create object
+let simon = new Person('Simon');
+let marta = new Person('Marta');
+
+//listen to event
+simon.on('name', () => {
+    console.log('my name is ' + simon.name);
+
+});
+
+marta.on('name', () => {
+    console.log('my name is ' + marta.name);
+
+});
+
+simon.emit('name');
+marta.emit('name');
