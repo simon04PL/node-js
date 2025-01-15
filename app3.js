ee83f4a4b491117449dc12ae99e57decc95b7e8e
simon04PL <szymon.msa@gmail.com>
2025-01-15 18:16:05 +0100

    15-01-2025 readline

---
+++ app3.js
@@ -0,0 +1,6 @@
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin, 
+                                    output : process.stdout});
+
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
