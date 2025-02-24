53ab9dd283cc25f1666f67903c698f795ba27c02
simon04PL <szymon.msa@gmail.com>
2025-02-24 12:30:08 +0100

    24-02-2025 login

--- 17-login.js
+++ 17-login.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'static', 'login.html'));
 });
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+    res.send('Successfully posted data');
+});
+
+app.listen(8000);
