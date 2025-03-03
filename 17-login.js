53afa6a045596e6576f9a806c6c9f9f57d58c641
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-03-03 11:42:50 +0100

    fix an error

--- 17-login.js
+++ 17-login.js
@@ -5,27 +5,45 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'login.html'));
 });
 
-app.post('/', (req, res)=>{
-    console.log(req.body);
-    const schema = Joi.object().keys({
+app.post('/', (req, res) => {
+    console.log('Received body:', req.body);
+
+    // Sprawdzenie czy req.body jest tablicą
+    let parsedBody;
+    if (Array.isArray(req.body)) {
+        parsedBody = req.body.reduce((acc, item) => {
+            acc[item.name] = item.value;
+            return acc;
+        }, {});
+    } else {
+        parsedBody = req.body;
+    }
+
+    console.log('Parsed body:', parsedBody);
+
+    const schema = Joi.object({
         email: Joi.string().trim().email().required(),
         password: Joi.string().min(5).max(10).required()
     });
-    Joi.validate(req.body, schema, (err, result)=>{
-        if(err){
-            console.log(err);
-            res.send('Error');
-        }
-        console.log(result);
-        res.send('Success');
-    });
+
+    const { error, value } = schema.validate(parsedBody);
+
+    if (error) {
+        console.log(error);
+        return res.status(400).send('Validation error');
+    }
+
+    console.log('Validated data:', value);
+    res.send('Success');
 });
 
-app.listen(8000);
+app.listen(8000, () => {
+    console.log('Server running on http://localhost:8000');
+});
