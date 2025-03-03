bcb401a48c704a15b6f23230aabe7149624a089f
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-03-03 11:38:17 +0100

    03.03.2025 login module with small error

--- 17-login.js
+++ 17-login.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'static', 'login.html'));
@@ -12,7 +14,18 @@ app.get('/', (req, res)=>{
 
 app.post('/', (req, res)=>{
     console.log(req.body);
-    res.send('Successfully posted data');
+    const schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
+    Joi.validate(req.body, schema, (err, result)=>{
+        if(err){
+            console.log(err);
+            res.send('Error');
+        }
+        console.log(result);
+        res.send('Success');
+    });
 });
 
 app.listen(8000);
