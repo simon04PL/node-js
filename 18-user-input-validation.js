2a128135625848e0f85e9adfd53a73b6894e11f5
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-03-11 11:32:54 +0100

    11-03.2025

--- 18-user-input-validation.js
+++ 18-user-input-validation.js
@@ -11,3 +11,28 @@ const userInput = {
     }, 
     preferences: arrayObjects
 };
+
+const personalInfoSchema = Joi.object().keys ({
+    streetAddress : Joi.string().trim().required(),
+    city : Joi.string().trim().required(),
+    state : Joi.string().trim().length(2).required()
+});
+
+const preferencesSchema = Joi.array().items(
+    Joi.object().keys({
+        example: Joi.string().required()
+    })
+);
+
+const schema = Joi.object().keys({
+    personalInfo : personalInfoSchema,
+    preferences : preferencesSchema
+});
+
+const { error, value } = schema.validate(userInput); 
+
+if (error) {
+    console.log(error);
+} else {
+    console.log(value);
+}
